c333347b33119103123575c72e4064a1ec0f5c4a
Michelle Fegatelli <chellefig17@gmail.com>
2021-02-22 14:09:12 -0500

    Nasa Images

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,21 +1,51 @@
 // from data.js
-var rawData = data;
+var tableData = data;
 
-// Get a reference to the table body
-var tableBody = d3.select("tbody");
+//populate data information into the HTML table
+var tbody = d3.select("tbody");
 
-// Define function to clear table and add new data
-function clearBuildTable(dataToBeInserted) {
-    tableBody.html("");
-    // Adding all the rows and cells
-    dataToBeInserted.forEach((ufoSighting) => {
-        var row = tableBody.append("tr");
-        Object.entries(ufoSighting).forEach(([key, value]) => {
+//UF sighting vlues for each column
+var displayAll = tableData.forEach((alienData) => {
+    console.log(alienData);
+    //append on table row 'tr' fpr each UF sighting object
+    var row = tbody.append("tr");
+    //use object entries to show each UFO sighting 
+    Object.entries(alienData).forEach(([key, value]) => {
+        //append the cell of for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+//select the button 
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    //clear out the html when the function is run????
+    tbody.html("");
+
+    //select the input date
+    var inputElement = d3.select("#input");
+    //get the value of th input date state
+    var inputValue = inputElement.property("value");
+    //filter the data that is equal to the date and time
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
+      sighting.city === inputValue ||
+      sighting.state === inputValue ||
+      sighting.country === inputValue ||
+      sighting.shape === inputValue);
+
+    filteredData.forEach(function(selections) {
+        //append one table row 'tr' for each ufo sighting for the specific date
+        var row = tbody.append("tr");
+        //use 'object.entries'
+        Object.entries(selections).forEach(function([key,value]){
+            //append a cell to the row for each value
             var cell = row.append("td");
             cell.text(value);
+
         });
+
+
     });
-};
-
-clearBuildTable(rawData);
-
+});
